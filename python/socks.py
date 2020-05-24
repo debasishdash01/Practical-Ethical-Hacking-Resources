a500222e9bb8bd0404f429db669b38969b18a260
Debasish Dash <debasish.11701128@lpu.in>
2020-05-24 15:33:29 +0530

    Update socks.py

--- python/socks.py
+++ python/socks.py
@@ -7,3 +7,7 @@ port = 4445
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(host,port)
+
+#Think of AF_INET as IP address
+#Think of SOCK_STREAM as a Port
+#This will just make a connection, after that it will quickly terminate as we didnt tell it to be persistent.
